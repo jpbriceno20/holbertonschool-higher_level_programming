ded2e07636c949370a2fb4417308f037ae957ea6
Juan Pablo <jpbriceno2001@gmail.com>
2025-05-12 02:34:21 +0000

    Python - import & modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in dir(hidden_4):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
