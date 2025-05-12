00003981180c85a2a43d308db6e890f6d9dbf2fd
Juan Pablo <jpbriceno2001@gmail.com>
2025-05-12 01:43:43 +0000

    Correction

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-new_str = str[39:66] + " " +  str[-17:]
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+new_str = str[39:66] + " " + str[80:92]
 print(new_str)
